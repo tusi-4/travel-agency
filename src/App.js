70918fe2ede422e13d84cf6c72214d4d8f468d2d
Marta Wojdat <marjow4@gmail.com>
2021-09-12 17:50:33 +0200

    Add multiple dramas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import MainLayout from './components/layout/MainLayout/MainLayout';
@@ -8,11 +8,17 @@ import MainLayout from './components/layout/MainLayout/MainLayout';
 import Home from './components/views/Home/Home';
 import Trips from './components/views/Trips/TripsContainer';
 // TODO - import other views
+import Trip from './components/views/Trip/TripContainer';
+import Countries from './components/views/Countries/CountriesContainer';
+import Country from './components/views/Country/CountryContainer';
+import Regions from './components/views/Regions/RegionsContainer';
 import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
 
 import parseTrips from './utils/parseTrips';
 import {setMultipleStates} from './redux/globalRedux';
+import {AnimatedSwitch} from 'react-router-transition';
+import styles from './styles/App.scss';
 
 class App extends React.Component {
   static propTypes = {
@@ -37,13 +43,23 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <MainLayout>
-          <Switch location={location}>
+          <AnimatedSwitch
+            atEnter={{ opacity: 1, top: 200 }}
+            atLeave={{ opacity: 0, top: 0 }}
+            atActive={{ opacity: 1, top: 0 }}
+            location={location}
+            className={styles.switchWrapper}
+          >
             <Route exact path='/' component={Home} />
             <Route exact path='/trips' component={Trips} />
             {/* TODO - add more routes for other views */}
+            <Route exact path='/trip/:id' component={Trip} />
+            <Route exact path='/countries' component={Countries} />
+            <Route exact path='/country/:id' component={Country} />
+            <Route exact path='/regions' component={Regions} />
             <Route exact path='/info' component={Info} />
             <Route path='*' component={NotFound} />
-          </Switch>
+          </AnimatedSwitch>
         </MainLayout>
       </BrowserRouter>
     );
--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -12,10 +12,24 @@ export const getFilteredTrips = ({trips, filters}) => {
   }
 
   // TODO - filter by duration
+  if(filters.days){
+    const pattern = new RegExp(filters.days, 'i');
+    output = output.filter(trip => pattern.test(trip.name));
+  }
 
   // TODO - filter by tags
+  if(filters.tags){
+    const pattern = new RegExp(filters.tags, 'i');
+    output = output.filter(trip => pattern.test(trip.name));
+  }
 
   // TODO - sort by cost descending (most expensive goes first)
+  if(filters.cost){
+    const pattern = new RegExp(filters.cost, 'i');
+    output = output.filter(trip => pattern.test(trip.name));
+    // most expensive first
+    // output.sort(function(a, b){return b - a}); ???????
+  }
 
   return output;
 };
@@ -24,6 +38,7 @@ export const getTripById = ({trips}, tripId) => {
   const filtered = trips;
 
   // TODO - filter trips by tripId
+  filtered.filter(trip => trip.trip.id == tripId); // filtered.filter?? DRAMA
 
   console.log('filtering trips by tripId:', tripId, filtered);
   return filtered.length ? filtered[0] : {error: true};
@@ -31,9 +46,10 @@ export const getTripById = ({trips}, tripId) => {
 
 export const getTripsForCountry = ({trips}, countryCode) => {
   const filtered = trips;
-
+  
   // TODO - filter trips by countryCode
-
+  filtered.filter(trip => trip.country.code == countryCode); // DRAMA 2
+ 
   console.log('filtering trips by countryCode:', countryCode, filtered);
   return filtered.length ? filtered : [{error: true}];
 };
