b6cac5def93615ebc771c337b268e8b6414a2a4c
Marta Wojdat <marjow4@gmail.com>
2021-09-25 18:11:55 +0200

    Add more TripSummary tests

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -10,4 +10,20 @@ describe('Component TripSummary', () => {
     const renderedAddress = link.find('.link').prop('to');
     expect(renderedAddress).toEqual(expectedAddress);
   });
+
+  it('should render correct image', () => {
+    const expectedSrc = 'image.jpg';
+    const expectedAlt = 'Lorem';
+    const image = shallow(<TripSummary image={expectedSrc} name={expectedAlt} />);
+    expect(image.find('.image').prop('src', 'alt')).toEqual(expectedSrc, expectedAlt);
+  });
+
+  it('should render props correctly', () => {
+    const component = shallow(<TripSummary name='Drama' cost='Ramad' days='Amadr' />);
+    expect(component).toBeTruthy();
+  });
+
+  it('should throw error when a prop is missing', () => {
+    expect(() => shallow(<TripSummary />)).toThrow();
+  });
 });
