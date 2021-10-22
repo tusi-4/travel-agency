4bd5669ae7f426516a7e9ea01310518ec36d3282
Marta Wojdat <marjow4@gmail.com>
2021-10-22 16:01:55 +0200

    Improve formatTime function in utils

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -2,15 +2,10 @@ export const formatTime = arg => {
   if(arg == undefined || isNaN(arg) || arg < 0){
     return null;
   } else {
-    // super autorska metoda
-    let ss = Math.floor(arg % 60);
-    let mm = Math.floor((arg / 60) % 60);
-    let hh = Math.floor((arg / 3600) % 60);
-    
-    let sec = ss.toString().padStart(2, '0');
-    let min = mm.toString().padStart(2, '0');
-    let hrs = hh.toString().padStart(2, '0');
+    const ss = Math.floor(arg % 60).toString().padStart(2, '0');
+    const mm = Math.floor((arg / 60) % 60).toString().padStart(2, '0');
+    const hh = Math.floor((arg / 3600) % 60).toString().padStart(2, '0');
 
-    return hrs + ':' + min + ':' + sec;
+    return hh + ':' + mm + ':' + ss;
   }
 };
